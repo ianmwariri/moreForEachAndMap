265dac240b00c89a0afd506b5d6b6586fdd72df5
smokeyice <mwangi.ian3@gmail.com>
2018-08-08 16:45:01 +0300

    Add For Each & Map

---
+++ js/script.js
@@ -0,0 +1,31 @@
+/*var numbers = [1,2,3,4,5,6];
+var doubledNumbers = [];
+numbers.forEach(function(number){
+	doubledNumbers.push(number * 2);
+});
+alert(doubledNumbers);
+
+*/
+
+/*var numbers = [1,2,3,4,5,6];
+var doubledNumbers = numbers.map(function(number){
+return number *2;
+});
+
+alert(doubledNumbers);
+
+*/
+
+/*var digits = [3,5,7,9];
+var plusOne= digits.map(function(digits){
+return digits +1;
+});
+alert(plusOne);
+
+*/
+
+var maneno = ['Greatness', 'Bold', 'Charisma'];
+var capsManenos = maneno.map(function (maneno) {
+  return maneno.toUpperCase();
+});
+alert(capsManenos);
